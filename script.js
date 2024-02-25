dbe0ea247434d0c2631c3bfa418c1a0c27666785
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2024-02-25 00:30:56 -0500

    about

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
+/*=== accordion ===*/
+const accordionSections = document.querySelectorAll('.accordion-section');
+
+accordionSections.forEach(section => {
+    section.addEventListener('click', function () {
+        this.classList.toggle('active');
+
+        accordionSections.forEach(otherSection => {
+            if (otherSection !== this && otherSection.classList.contains('active')) {
+                otherSection.classList.remove('active');
+            }
+        });
+    });
+});
