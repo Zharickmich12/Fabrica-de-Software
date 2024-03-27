cc8fed0b41f1c97a64b91aca81267210f2d9e185
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2024-03-26 20:39:06 -0500

    scollreveal

--- script.js
+++ script.js
@@ -93,11 +93,21 @@ function googleTranslateElementInit(){
 }
 
 //=== scroll reveal ===//
-ScrollReveal({ 
-  reset: true,
-  distance: '60px',
-  duration: 2000,
-  delay: 200
-});
+window.addEventListener('scroll', reveal);
+function reveal () {
+  var reveals = document.querySelectorAll('.reveal');
 
-ScrollReveal().reveal('.home-content', '.heading', { origin: 'top' });
+  for(var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+
+  }
+}
